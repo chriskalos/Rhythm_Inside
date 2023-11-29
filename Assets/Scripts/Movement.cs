5b1d04aee7b8c3c57d713ac5992e969d434660a1
chriskalos <chriskalos658@gmail.com>
2023-11-29 23:38:37 +0200

    Switched from 2D project to 3D project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintSpeed;
+    private Rigidbody _rigidbody;
+    private Vector3 _inputVector;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float dx = Input.GetAxisRaw("Horizontal");
+        float dz = Input.GetAxisRaw("Vertical");
+
+        _inputVector = new Vector3(dx, 0, dz).normalized;
+        
+        // Check if sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _inputVector *= sprintSpeed;
+        }
+        else
+        {
+            _inputVector *= walkSpeed;
+        }
+
+        // Flip the character if moving left or right
+        if (dx != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(dx), 1, 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = _inputVector * Time.fixedDeltaTime;
+        _rigidbody.MovePosition(_rigidbody.position + moveDirection);
+    }
+}
