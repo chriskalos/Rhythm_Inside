81d4801f5ff4edd69b7cc47888f9073909de5ec0
chriskalos <chriskalos658@gmail.com>
2023-12-01 00:47:10 +0200

    Placeholder model & Camera tracking!!

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float smoothTime = 0.25f;
+    private Vector3 _offset;
+    private Vector3 _velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = new Vector3(0f, 1.5f, -7f);
+        _velocity = Vector3.zero;
+    }
+
+    void LateUpdate()
+    {
+        var position = playerTransform.position;
+        Vector3 targetPosition = new Vector3(position.x + _offset.x, position.y + _offset.y, _offset.z);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+        transform.position = smoothedPosition;
+    }
+}
