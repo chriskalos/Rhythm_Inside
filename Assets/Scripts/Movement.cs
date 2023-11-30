36f9e8e2d197d3c584bb212c9f7a92c10f4f01fd
chriskalos <chriskalos658@gmail.com>
2023-11-30 21:36:09 +0200

    Fixed Movement.cs, thanks chatto

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,48 +1,47 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float walkSpeed;
     [SerializeField] private float sprintSpeed;
+    private CharacterController _characterController;
     private Rigidbody _rigidbody;
     private Vector3 _inputVector;
+    private Vector3 _velocity; // Variable to store the vertical velocity
 
     void Start()
     {
+        _characterController = GetComponent<CharacterController>();
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         float dx = Input.GetAxisRaw("Horizontal");
-        float dy = _rigidbody.velocity.y; // Keep the existing vertical velocity
         float dz = Input.GetAxisRaw("Vertical");
 
         _inputVector = new Vector3(dx, 0, dz).normalized;
-        
+
         // Check if sprinting
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            _inputVector *= sprintSpeed;
-        }
-        else
-        {
-            _inputVector *= walkSpeed;
-        }
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+        Vector3 moveDirection = _inputVector * currentSpeed;
         
-        // Apply the existing y velocity (falling or jumping)
-        _inputVector.y = dy;
+        _velocity.y += Physics.gravity.y * Time.deltaTime; // Apply gravity
+        
+        // Combine movement and gravity, then move the character
+        _characterController.Move((moveDirection + _velocity) * Time.deltaTime);
 
         // Flip the character if moving left or right
         if (dx != 0)
         {
-            transform.localScale = new Vector3(Mathf.Sign(dx), transform.localScale.y, transform.localScale.z);
+            // Introducing a variable to make this access efficient
+            // Dear Professor, JetBrains Rider told me to do this
+            // and it makes sense, so wherever you see this, know that
+            // my IDE is telling me to do it.
+            var localScale = transform.localScale;
+            localScale = new Vector3(Mathf.Sign(dx), localScale.y, localScale.z);
+            transform.localScale = localScale;
         }
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 moveDirection = _inputVector * Time.fixedDeltaTime;
-        _rigidbody.MovePosition(_rigidbody.position + moveDirection);
     }
 }
