7c960a937e141236bde9bd8e91f837c14c2c8cf4
chriskalos <chriskalos658@gmail.com>
2023-12-10 21:39:11 +0200

    Finalize battle sprite

--- Assets/Scripts/PelletScroller.cs
+++ Assets/Scripts/PelletScroller.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PelletScroller : MonoBehaviour
 {
+    [SerializeField] private GameObject rhythmAttackPanel;
     [SerializeField] private GameObject pelletPrefab;
     
     // Start is called before the first frame update
@@ -21,16 +22,21 @@ public class PelletScroller : MonoBehaviour
 
     public void SpawnPellet()
     {
-        GameObject instance = Instantiate(pelletPrefab, new Vector3(600f, 0f, -10f), Quaternion.identity);
-        
-        instance.transform.SetParent(this.transform, false);
+        if (rhythmAttackPanel.activeSelf)
+        {
+            GameObject instance = Instantiate(pelletPrefab, new Vector3(610f, 0f, -10f), Quaternion.identity);
+            instance.transform.SetParent(this.transform, false);
+        }
     }
 
     public void MoveToTheBeat()
     {
-        foreach (BeatPellet pellet in GetComponentsInChildren<BeatPellet>())
+        if (rhythmAttackPanel.activeSelf)
         {
-            pellet.transform.localPosition += new Vector3(-5f, 0f, 0f);
+            foreach (BeatPellet pellet in GetComponentsInChildren<BeatPellet>())
+            {
+                pellet.transform.localPosition += new Vector3(-5f, 0f, 0f);
+            }
         }
     }
 }
